ae39fb8734825dfdf2fb9dda745079614f6230d2
Sarah Stodder <sjsarahboo@gmail.com>
2023-10-16 15:04:39 -0600

    added some code

--- questions.py
+++ questions.py
@@ -2,7 +2,7 @@
 # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
 
 def hello_name(user_name):
-    pass
+    print("hello")
      
                
 # Question 2
